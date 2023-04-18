5e0c690857f707a5e4ba9437a29921d4f9489b23
Pherom <pheromem@gmail.com>
2023-04-18 15:56:45 +0300

    Edited text to print in Hello.java again

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Here's some updated text to print! :D" + i);
+            System.out.println("Edited once again! :)" + i);
         }
     }
 }
